ae93c9fac21400ad16ac1bf6927cf62e239c8c17
Daniel Tozato <danieltozatoz@gmail.com>
2026-09-16 13:37:16 -0300

    mudança no acervo/models

--- acervo/models.py
+++ acervo/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
-    autor = models.CharField(max_length=100)
+    autor = models.ForeignKey(
+        Autor,
+        on_delete=models.PROTECT
+    )
     ano = models.IntegerField()
-    disponivel = models.BooleanField(default=True)
 
     def __str__(self):
         return self.titulo
+    
+class Exemplar(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    codigo_patrimonio = models.CharField(max_length=20, unique=True)
+    disponivel = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.livro.titulo} - Exemplar {self.id}"
--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'acervo',
+    'usuarios',
+    'emprestimos',
 ]
 
 MIDDLEWARE = [
