78f8ec49aaccb44f3ed5ecc7424ddac7692a845e
Daniel Tozato <danieltozatoz@gmail.com>
2026-09-16 13:52:55 -0300

    usuarios\models

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return self.nome
