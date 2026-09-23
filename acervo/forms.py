30da19d2bc8a0a562a7a78d2681f04e392423f67
Daniel Tozato <danieltozatoz@gmail.com>
2026-09-23 20:44:47 -0300

    Feature 2

--- acervo/forms.py
+++ acervo/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from datetime import date
 from .models import Livro, Autor, Exemplar
 
 class AutorForm(forms.ModelForm):
@@ -10,6 +12,13 @@ class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['titulo', 'autor', 'ano']
+
+    def clean_ano(self):
+        ano = self.cleaned_data.get('ano')
+        ano_atual = date.today().year
+        if ano and ano > ano_atual:
+            raise ValidationError('O ano de publicação não pode ser um ano futuro.')
+        return ano
         
 class ExemplarForm(forms.ModelForm):
     class Meta:
